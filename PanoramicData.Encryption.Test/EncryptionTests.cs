a81f049347eb0147bb4e72dcc123b938c69b909e
David Bond <david.bond@panoramicdata.com>
2022-04-04 19:20:06 +0100

    Code tidy

--- PanoramicData.Encryption.Test/EncryptionTests.cs
+++ PanoramicData.Encryption.Test/EncryptionTests.cs
@@ -1,169 +1,169 @@
-using FluentAssertions;
 using System;
 using System.Security.Cryptography;
+using FluentAssertions;
 using Xunit;
 
 namespace PanoramicData.Encryption.Test
 {
-    public class EncryptionTests
-    {
-        private const string PlainText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi.";
-        private const string EncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF";
-        private const string BadEncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEF£";
-        private const string WrongEncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFE";
-        private const string ShortEncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEF";
-        private const string WrongSalt = "00112233445566778899AABBCCDDEEFF";
-        private const string TooShortSalt = "001122334455667788";
-        private readonly string _cipherText;
-        private readonly string _salt;
-        private readonly EncryptionService _goodSecurityService;
-        private readonly EncryptionService _badSecurityService;
+	public class EncryptionTests
+	{
+		private const string PlainText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi. Donec euismod, nisl eget consectetur consectetur, nisi nisi aliquet nunc, euismod aliquam nunc nisi euismod nisi.";
+		private const string EncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF";
+		private const string BadEncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEF£";
+		private const string WrongEncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFE";
+		private const string ShortEncryptionKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEF";
+		private const string WrongSalt = "00112233445566778899AABBCCDDEEFF";
+		private const string TooShortSalt = "001122334455667788";
+		private readonly string _cipherText;
+		private readonly string _salt;
+		private readonly EncryptionService _goodSecurityService;
+		private readonly EncryptionService _badSecurityService;
 
-        public EncryptionTests()
-        {
-            _goodSecurityService = new EncryptionService(EncryptionKey);
-            _badSecurityService = new EncryptionService(WrongEncryptionKey);
-            (_cipherText, _salt) = _goodSecurityService.Encrypt(PlainText);
-        }
+		public EncryptionTests()
+		{
+			_goodSecurityService = new EncryptionService(EncryptionKey);
+			_badSecurityService = new EncryptionService(WrongEncryptionKey);
+			(_cipherText, _salt) = _goodSecurityService.Encrypt(PlainText);
+		}
 
-        /// <summary>
-        /// The encryption key should not be null
-        /// </summary>
-        [Fact]
-        public void NullEncryptionKey_Fails()
-            => string.Empty.Invoking(o => new EncryptionService(null!))
-                .Should()
-                .Throw<ArgumentNullException>();
+		/// <summary>
+		/// The encryption key should not be null
+		/// </summary>
+		[Fact]
+		public void NullEncryptionKey_Fails()
+			=> string.Empty.Invoking(o => new EncryptionService(null!))
+				.Should()
+				.Throw<ArgumentNullException>();
 
-        /// <summary>
-        /// A short encryption key should throw an ArgumentException
-        /// </summary>
-        [Fact]
-        public void ShortEncryptionKey_Fails()
-            => string.Empty.Invoking(o => new EncryptionService(ShortEncryptionKey))
-                .Should()
-                .Throw<ArgumentException>();
+		/// <summary>
+		/// A short encryption key should throw an ArgumentException
+		/// </summary>
+		[Fact]
+		public void ShortEncryptionKey_Fails()
+			=> string.Empty.Invoking(o => new EncryptionService(ShortEncryptionKey))
+				.Should()
+				.Throw<ArgumentException>();
 
-        /// <summary>
-        /// A bad encryption key should throw an ArgumentException
-        /// </summary>
-        [Fact]
-        public void BadEncryptionKey_Fails()
-            => string.Empty.Invoking(o => new EncryptionService(BadEncryptionKey))
-                .Should()
-                .Throw<ArgumentException>();
+		/// <summary>
+		/// A bad encryption key should throw an ArgumentException
+		/// </summary>
+		[Fact]
+		public void BadEncryptionKey_Fails()
+			=> string.Empty.Invoking(o => new EncryptionService(BadEncryptionKey))
+				.Should()
+				.Throw<ArgumentException>();
 
-        /// <summary>
-        /// The cipher text and salt should be not be null, empty or the plain text
-        /// </summary>
-        [Fact]
-        public void Basics_Succeed()
-        {
-            CheckCipherOrSalt(_cipherText);
-            CheckCipherOrSalt(_salt);
-        }
+		/// <summary>
+		/// The cipher text and salt should be not be null, empty or the plain text
+		/// </summary>
+		[Fact]
+		public void Basics_Succeed()
+		{
+			CheckCipherOrSalt(_cipherText);
+			CheckCipherOrSalt(_salt);
+		}
 
-        private static void CheckCipherOrSalt(string salt)
-        {
-            salt.Should().NotBeNullOrEmpty();
-            salt.Should().NotBe(PlainText);
-        }
+		private static void CheckCipherOrSalt(string salt)
+		{
+			salt.Should().NotBeNullOrEmpty();
+			salt.Should().NotBe(PlainText);
+		}
 
-        /// <summary>
-        /// Using the right salt should succeed
-        /// </summary>
-        [Fact]
-        public void EncryptDecryptCycle_Succeeds()
-            => _goodSecurityService
-                .Decrypt(_cipherText, _salt)
-                .Should()
-                .Be(PlainText);
+		/// <summary>
+		/// Using the right salt should succeed
+		/// </summary>
+		[Fact]
+		public void EncryptDecryptCycle_Succeeds()
+			=> _goodSecurityService
+				.Decrypt(_cipherText, _salt)
+				.Should()
+				.Be(PlainText);
 
-        /// <summary>
-        /// Using null salt should throw
-        /// </summary>
-        [Fact]
-        public void NullSalt_Throws()
-            => _goodSecurityService
-                .Invoking(ss => ss.Decrypt(_cipherText, null!))
-                .Should().Throw<ArgumentNullException>();
+		/// <summary>
+		/// Using null salt should throw
+		/// </summary>
+		[Fact]
+		public void NullSalt_Throws()
+			=> _goodSecurityService
+				.Invoking(ss => ss.Decrypt(_cipherText, null!))
+				.Should().Throw<ArgumentNullException>();
 
-        /// <summary>
-        /// Using null cipherText should throw an ArgumentNullException
-        /// </summary>
-        [Fact]
-        public void NullCipherText_Throws()
-            => _goodSecurityService
-                .Invoking(ss => ss.Decrypt(null!, _salt))
-                .Should().Throw<ArgumentNullException>();
+		/// <summary>
+		/// Using null cipherText should throw an ArgumentNullException
+		/// </summary>
+		[Fact]
+		public void NullCipherText_Throws()
+			=> _goodSecurityService
+				.Invoking(ss => ss.Decrypt(null!, _salt))
+				.Should().Throw<ArgumentNullException>();
 
-        /// <summary>
-        /// Using too short a salt to encrypt should throw an ArgumentException
-        /// </summary>
-        [Fact]
-        public void EncryptWithPreviousSalt_Succeeds()
-        {
-            (var cipherText, var salt) = _goodSecurityService.Encrypt(PlainText, _salt);
-            salt.Should().Be(_salt);
-            cipherText.Should().Be(_cipherText);
-            _goodSecurityService
-                .Decrypt(cipherText, salt)
-                .Should()
-                .Be(PlainText);
-        }
+		/// <summary>
+		/// Using too short a salt to encrypt should throw an ArgumentException
+		/// </summary>
+		[Fact]
+		public void EncryptWithPreviousSalt_Succeeds()
+		{
+			(var cipherText, var salt) = _goodSecurityService.Encrypt(PlainText, _salt);
+			salt.Should().Be(_salt);
+			cipherText.Should().Be(_cipherText);
+			_goodSecurityService
+				.Decrypt(cipherText, salt)
+				.Should()
+				.Be(PlainText);
+		}
 
-        /// <summary>
-        /// Using too short a salt to encrypt should throw an ArgumentException
-        /// </summary>
-        [Fact]
-        public void EncryptWithShortSalt_Throws()
-            => _goodSecurityService
-                .Invoking(ss => ss.Encrypt(PlainText, TooShortSalt))
-                .Should().Throw<ArgumentException>();
+		/// <summary>
+		/// Using too short a salt to encrypt should throw an ArgumentException
+		/// </summary>
+		[Fact]
+		public void EncryptWithShortSalt_Throws()
+			=> _goodSecurityService
+				.Invoking(ss => ss.Encrypt(PlainText, TooShortSalt))
+				.Should().Throw<ArgumentException>();
 
-        /// <summary>
-        /// Using too short a salt to decrypt should throw an ArgumentException
-        /// </summary>
-        [Fact]
-        public void DecryptWithShortSalt_Throws()
-            => _goodSecurityService
-                .Invoking(ss => ss.Decrypt(_cipherText, TooShortSalt))
-                .Should().Throw<ArgumentException>();
+		/// <summary>
+		/// Using too short a salt to decrypt should throw an ArgumentException
+		/// </summary>
+		[Fact]
+		public void DecryptWithShortSalt_Throws()
+			=> _goodSecurityService
+				.Invoking(ss => ss.Decrypt(_cipherText, TooShortSalt))
+				.Should().Throw<ArgumentException>();
 
-        /// <summary>
-        /// Using the wrong salt should partially fail (see https://stackoverflow.com/a/71740594/6508058)
-        /// </summary>
-        [Fact]
-        public void WrongSalt_PartiallyFails()
-        {
-            // Using the wrong salt should fail
-            var result1 = _goodSecurityService
-                    .Decrypt(_cipherText, WrongSalt);
+		/// <summary>
+		/// Using the wrong salt should partially fail (see https://stackoverflow.com/a/71740594/6508058)
+		/// </summary>
+		[Fact]
+		public void WrongSalt_PartiallyFails()
+		{
+			// Using the wrong salt should fail
+			var result1 = _goodSecurityService
+					.Decrypt(_cipherText, WrongSalt);
 
-            result1
-            .Should()
-            .NotBeNull();
+			result1
+			.Should()
+			.NotBeNull();
 
-            result1
-            .Should()
-            .NotBe(PlainText);
+			result1
+			.Should()
+			.NotBe(PlainText);
 
-            result1
-            .Should()
-            .EndWith(PlainText[16..]);
-        }
+			result1
+			.Should()
+			.EndWith(PlainText[16..]);
+		}
 
-        /// <summary>
-        /// Using the wrong encryption key should fail
-        /// </summary>
-        [Fact]
-        public void WrongEncryptionKey_ReturnsNull()
-        {
-            // Using the wrong encryption key should fail
-            var result2 = _badSecurityService
-                .Invoking(ss => ss.Decrypt(_cipherText, _salt))
-                .Should()
-                .Throw<CryptographicException>();
-        }
-    }
+		/// <summary>
+		/// Using the wrong encryption key should fail
+		/// </summary>
+		[Fact]
+		public void WrongEncryptionKey_ReturnsNull()
+		{
+			// Using the wrong encryption key should fail
+			var result2 = _badSecurityService
+				.Invoking(ss => ss.Decrypt(_cipherText, _salt))
+				.Should()
+				.Throw<CryptographicException>();
+		}
+	}
 }
--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-PanoramicData.EncryptionService
+# PanoramicData.Encryption
+
+Although you could use the nuget package, why would you trust it?
+Better to read and understand and copy the source into your project.
+
+Seriously, there's only one file.  Copy it.  It's [here](https://raw.githubusercontent.com/panoramicdata/PanoramicData.Encryption/main/PanoramicData.Encryption/EncryptionService.cs).
+
+To use:
+
+```C#
+   using PanoramicData.Encryption;
+
+   // Preparation
+
+   // Here is the plaintext we want to protect:
+   var plaintext = "Hello, World!";
+   
+   // The encryption key should be a 32 byte array,
+   // represented as a 64 character hex string.
+   // NO NOT use this one.  It will work, but it is not secure.
+   // DO NOT store your encryption keys in your source code.
+   // Environment variables are one way to keep your data, keys and source code separate.
+   // You could use var privateKey = Environment.GetEnvironmentVariable("PrivateKey") method to retrieve this.
+   // So we have a working example, let's put it in code for now.
+   var privateKey = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF";
+
+   // Create the encryption service using the private key.
+   var encryptionService = new EncryptionService(privateKey);
+
+
+   // Encryption
+
+   // EITHER 1) create cipherText and salt at the same time.
+   // This is the recommended way to do it.
+   (var cipherText, var salt) = encryptionService.Encrypt(plaintext);
+
+   // OR 2) create your own salt first:
+   var salt = "00112233445566778899AABBCCDDEEFF";
+   (var cipherText2, var _) = encryptionService.Encrypt(plaintext, salt);
+
+   // Storage
+   // You should store the salt and cipherText in your database.
+   // You may prefer to Base64 encode them before storing them, for storage efficiency.
+
+   // ...
+
+   // Retrieval
+   // The salt and cipherText are safe for anyone to have access to.
+   // Without the private key, no-one cannot decrypt the data.
+   // Retrieve them from storage...
+
+   // Decryption
+
+   // You can decrypt the cipherText using the EncryptionService and the salt:
+   var restoredPlainText = encryptionService.Decrypt(cipherText, salt);
+
+   Console.WriteLine(restoredPlainText);
+
+   // Worst.  "Hello, World!"  Ever.
+```
